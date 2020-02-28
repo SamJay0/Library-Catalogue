65d3cb4bc926449f70491f3a4fccae1344eb2af0
SamJay0 <samjay0.254@gmail.com>
2020-02-28 16:24:40 +0300

    recyclerview adapter implementation

--- app/src/main/java/com/example/librarycatalogue/adapters/BookSectionAdapter.kt
+++ app/src/main/java/com/example/librarycatalogue/adapters/BookSectionAdapter.kt
@@ -1,23 +1,35 @@
 package com.example.librarycatalogue.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.librarycatalogue.R
+import com.example.librarycatalogue.data.BookSection
 
-class BookSectionAdapter : RecyclerView.Adapter<BookSectionAdapter.BookSectionViewHolder>() {
+class BookSectionAdapter(private  val bookSections: List<BookSection>) : RecyclerView.Adapter<BookSectionAdapter.BookSectionViewHolder>() {
 
-    class BookSectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class BookSectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val bookSectionImage:ImageView = itemView.findViewById(R.id.book_section_image)
+
+        fun bind(bookSection: BookSection){
+            bookSectionImage.setImageResource(bookSection.imageResource)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookSectionViewHolder {
-        TODO("not implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.book_section_list_item, parent,false)
+        return BookSectionViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented")
+        return bookSections.size
     }
 
     override fun onBindViewHolder(holder: BookSectionViewHolder, position: Int) {
-        TODO("not implemented")
+        holder.bind(bookSections[position])
     }
 
 
