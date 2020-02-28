75d0d23244771c960d8ee1c2976af59085b0565d
SamJay0 <samjay0.254@gmail.com>
2020-02-28 16:42:09 +0300

    fragment oncreate,layout manager

--- app/src/main/java/com/example/librarycatalogue/HomeFragment.kt
+++ app/src/main/java/com/example/librarycatalogue/HomeFragment.kt
@@ -6,19 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.librarycatalogue.adapters.BookSectionAdapter
+import com.example.librarycatalogue.data.BookSection
 import com.example.librarycatalogue.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var bookSectionRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
-        val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater ,R.layout.fragment_home,container,false)
+        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
+                    inflater ,R.layout.fragment_home,container,false)
+
+        bookSectionRecyclerView.layoutManager = GridLayoutManager(this.context,2)
+        bookSectionRecyclerView.adapter = BookSectionAdapter(bookSectionList())
 
         return binding.root
 
     }
+   fun bookSectionList():List<BookSection>{
+       val book_sections= listOf<BookSection>(
+           BookSection(R.drawable.comic),
+           BookSection(R.drawable.comic),
+           BookSection(R.drawable.comic),
+           BookSection(R.drawable.comic),
+           BookSection(R.drawable.comic),
+           BookSection(R.drawable.comic)
+       )
+       return book_sections
+   }
 
 
 }
