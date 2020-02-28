17f08c917c4cf114b526ef8962576c2afe1480a1
SamJay0 <samjay0.254@gmail.com>
2020-02-28 15:54:51 +0300

    adapter,viewholder

---
+++ app/src/main/java/com/example/librarycatalogue/adapters/BookSectionAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.librarycatalogue.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BookSectionAdapter : RecyclerView.Adapter<BookSectionAdapter.BookSectionViewHolder>() {
+
+    class BookSectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookSectionViewHolder {
+        TODO("not implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented")
+    }
+
+    override fun onBindViewHolder(holder: BookSectionViewHolder, position: Int) {
+        TODO("not implemented")
+    }
+
+
+}
---
+++ app/src/main/java/com/example/librarycatalogue/data/BookSection.kt
@@ -0,0 +1,3 @@
+package com.example.librarycatalogue.data
+
+data class BookSection(var imageResource:Int)
